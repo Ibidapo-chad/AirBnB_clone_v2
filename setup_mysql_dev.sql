d20c5c4cb8582b4ec0f626ac1a3197040a7a32c9
MillicentMal <millicentmalinga19@gmail.com>
2021-08-12 01:37:52 +0000

    Task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- prepares server for working 
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbhb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON 'hbhb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
