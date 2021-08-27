c5f156428d01aa3f8c6ce121a797c20eb3964234
Ibidapo-chad <Ibidapo.chad@gmail.com>
2021-08-27 00:22:56 +0000

    scripts created

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+Creates and distributes an archive to web servers
+"""
+
+from datetime import datetime
+from fabric.api import run, local, env, put
+from os.path import isfile, exists
+
+
+env.hosts = ['34.138.173.41','34.73.165.69']
+
+#!/usr/bin/python3
+"""
+Generate a .tgz archive from the contents of web_static folder
+The name of the archive created is:
+       web_static_<year><month><day><hour><minute><second>.tgz
+"""
+
+import os
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+                """ Creates a .tgz archive from the contents of the web_static folder """
+                        now = datetime.now()
+                                now_str = now.strftime("%Y%m%d%H%M%S")
+                                        now_str = now_str.replace('/', '')
+                                                if not os.path.exists('versions'):
+                                                                    local("mkdir versions")
+                                                                            tar_name = "versions/web_static_{}.tgz".format(now_str)
+                                                                                    result = local("tar -cvzf {} web_static".format(tar_name))
+                                                                                            if result.succeeded:
+                                                                                                                tar_name
+                                                                                            else:
+                                                                                                                return None
+#!/usr/bin/python3
+"""
+Distributes an archive to web servers
+"""
+
+from fabric.api import run, put, env
+from os.path import isfile
+
+
+env.hosts = ['34.138.173.41', '34.73.165.69']
+
+
+def do_deploy(archive_path):
+                """ Distributes an archive to web servers """
+                        if not isfile(archive_path):
+                                            return False
+                                            try:
+                                                                localpath = archive_path.split('/')[1]
+                                                                            newpath = localpath.split('.')[0]
+                                                                                        rempath = "/data/web_static/releases/"
+
+                                                                                                    put(archive_path, "/tmp/".format(localpath))
+                                                                                                                run("mkdir -p {}{}".format(rempath, newpath))
+                                                                                                                            run("tar -xzf /tmp/{} -C {}{}".format(localpath, rempath, newpath))
+                                                                                                                                        run("rm /tmp/{}".format(localpath))
+                                                                                                                                                    run("mv {0}{1}/web_static/* {0}{1}/".format(rempath, newpath))
+                                                                                                                                                                run("rm -rf {}{}/web_static".format(rempath, newpath))
+                                                                                                                                                                            run("rm -rf /data/web_static/current")
+                                                                                                                                                                                        run("ln -s {}{}/ /data/web_static/current".format(rempath, newpath))
+                                                                                                                                                                                                    return True
+                                            except:
+                                                                return False
+                                                                                                        
+                                                                                                        
+def deploy():
+        """ Call the do_pack() function and based on its return value,
+        call do_deploy or return False
+        """
+            result = do_pack()
+            if result:
+                return do_deploy(result)
